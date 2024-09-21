d12398e698b3230000bdd8b749a070a9b4e9b07d
Edmund Bates <edmundbates@Edmunds-MacBook-Pro.local>
2024-09-21 12:52:42 -0700

    Linting fixes

--- app/canvas-magic/dithering.ts
+++ app/canvas-magic/dithering.ts
@@ -1,7 +1,7 @@
 'use client'
 
 export const getDitheringImg = async () => {
-    const ditheringPromise = new Promise<string>((resolve, _reject) => {
+    const ditheringPromise = new Promise<string>((resolve) => {
         // run outside of main thread
         window.setTimeout(() => {
             const canvas = document.getElementById('dithering') as HTMLCanvasElement;
@@ -9,8 +9,8 @@ export const getDitheringImg = async () => {
             // therefore force non-null cuz I know I added that element :-D
             const context = canvas!.getContext('2d')!;
             context.fillStyle = 'rgba(0,0,0,.6)';
-            for (var x = 0; x < 20; x++) {
-                for (var y = 0; y < 20; y ++) {
+            for (let x = 0; x < 20; x++) {
+                for (let y = 0; y < 20; y ++) {
                     if ((x+y) % 2 === 0) {
                         context.fillRect(x, y, 1, 1);
                     }
--- app/canvas-magic/lines.ts
+++ app/canvas-magic/lines.ts
@@ -1,7 +1,7 @@
 'use client'
 
 export const getLinesImg = async () => {
-    const ditheringPromise = new Promise<string>((resolve, _reject) => {
+    const ditheringPromise = new Promise<string>((resolve) => {
         // run outside of main thread
         window.setTimeout(() => {
             const canvas = document.getElementById('lines') as HTMLCanvasElement;
@@ -9,8 +9,8 @@ export const getLinesImg = async () => {
             // therefore force non-null cuz I know I added that element :-D
             const context = canvas!.getContext('2d')!;
             context.fillStyle = 'rgba(0,0,0,.6)';
-            for (var x = 0; x < 20; x++) {
-                for (var y = 4; y < 16; y ++) {
+            for (let x = 0; x < 20; x++) {
+                for (let y = 4; y < 16; y ++) {
                     if ((y) % 2 === 0) {
                         context.fillRect(x, y, 1, 1);
                     }
