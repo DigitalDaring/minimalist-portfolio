cd58c8beca78bbf29d0bf5efd8eb3d3dc150e824
Edmund Bates <edmundbates@Edmunds-MacBook-Pro.local>
2024-09-21 14:03:54 -0700

    This patch adds a check for undefined window

--- app/canvas-magic/lines.ts
+++ app/canvas-magic/lines.ts
@@ -3,21 +3,26 @@
 export const getLinesImg = async () => {
     const ditheringPromise = new Promise<string>((resolve) => {
         // run outside of main thread
-        window.setTimeout(() => {
-            const canvas = document.getElementById('lines') as HTMLCanvasElement;
-            // I control the vertical, and the horizontal!
-            // therefore force non-null cuz I know I added that element :-D
-            const context = canvas!.getContext('2d')!;
-            context.fillStyle = 'rgba(0,0,0,.6)';
-            for (let x = 0; x < 20; x++) {
-                for (let y = 4; y < 16; y ++) {
-                    if ((y) % 2 === 0) {
-                        context.fillRect(x, y, 1, 1);
+        // @ts-ignore:next-line
+        if (typeof window !== "undefined") {
+            // Client-side-only code
+            window.setTimeout(() => {
+                const canvas = document.getElementById('lines') as HTMLCanvasElement;
+                // I control the vertical, and the horizontal!
+                // therefore force non-null cuz I know I added that element :-D
+                const context = canvas!.getContext('2d')!;
+                context.fillStyle = 'rgba(0,0,0,.6)';
+                for (let x = 0; x < 20; x++) {
+                    for (let y = 4; y < 16; y ++) {
+                        if ((y) % 2 === 0) {
+                            context.fillRect(x, y, 1, 1);
+                        }
                     }
                 }
-            }
-            resolve(canvas.toDataURL('image/gif'));
-        }, 0);
+                resolve(canvas.toDataURL('image/gif'));
+            }, 0);
+        }
+        
     });
 
     return ditheringPromise;
