dec7bb5f4b628ea285baa551df6be75ebbe1635f
Edmund Bates <edmundbates@Edmunds-MacBook-Pro.local>
2026-02-01 12:40:05 -0500

    This patch adds the dropdown menu items for the main header

--- app/components/main-header/main-header.component.tsx
+++ app/components/main-header/main-header.component.tsx
@@ -1,10 +1,71 @@
+import { useState } from 'react';
 import styles from './main-header.component.module.scss';
 
+enum NavHeader {
+    ABOUT = 'about',
+    EXPERIMENTS = 'experiments',
+    CV = 'cv'
+}
+
 const MainHeader = () => {
+
+    const [navState, setNavState] = useState({
+        [NavHeader.ABOUT]: false,
+        [NavHeader.EXPERIMENTS]: false,
+        [NavHeader.CV]: false
+    });
+
+    const toggleNav = (toToggle: NavHeader) => {
+        const isAlreadyOpen = navState[toToggle];
+        const newState = {
+            [NavHeader.ABOUT]: false,
+            [NavHeader.EXPERIMENTS]: false,
+            [NavHeader.CV]: false
+        }
+
+        // if the nav wasn't already open, open it!
+        if (!isAlreadyOpen) {
+            newState[toToggle] = true;
+        }
+
+        setNavState(newState);
+    };
+
+
     return <header className={styles.fixedHeader}>
-        <a>About</a>
-        <a>Experiments</a>
-        <a>Curriculum Vitae</a>
+        <nav onClick={() => toggleNav(NavHeader.ABOUT)}>
+            About
+            <ul className={navState[NavHeader.ABOUT] ? styles.expanded : ''}>
+                <li>
+                    About This Site
+                </li>
+                <li>
+                    About The Author
+                </li>
+            </ul>
+        </nav>
+        <nav onClick={() => toggleNav(NavHeader.EXPERIMENTS)}>
+            Experiments
+            <ul className={navState[NavHeader.EXPERIMENTS] ? styles.expanded : ''}>
+                <li>
+                    Music Visualizer
+                </li>
+                <li>
+                    Image Dots
+                </li>
+                <li>
+                    Live Video Filters
+                </li>
+            </ul>
+        </nav>
+        <nav onClick={() => toggleNav(NavHeader.CV)}>
+            Curriculum Vitae
+            <ul className={navState[NavHeader.CV] ? styles.expanded : ''}>
+                <li>
+                    Download PDF
+                </li>
+            </ul>
+        </nav>
     </header>
 }
 
